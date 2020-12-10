d916c676db4ad74a1b0da909da8621baa882a62a
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2020-12-10 19:22:55 +0000

    seriously urggghhhhh

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,19 +7,13 @@ let state = {};
 // Sets the game at 0 with state of all members of the High Council not pleased, no painted shield, and all other inventory items set to true
 
 function startGame() {
-    state = {
-
-    };
+    state = {};
     show = showTextNode(0);
 }
 
 // Once all members of the High Council are pleased, the game should move onto ID 300 and the win state
 
 function GameComplete() {
-    state = {
-        BjornPleased: true, 
-        AstridPleased: true
-    };
     show = showTextNode(300);
 }
 
@@ -51,19 +45,44 @@ function showOption(option) {
     return option.requiredState == null || option.requiredState(state);
 }
 
+var Bjorn = {
+    pleased: 0,
+};
+
+BjornPleased = Bjorn.pleased()
+
+var Astrid = {
+    pleased: 0,
+};
+
+AstridPleased = Astrid.pleased()
+
+// Before I completely forget what I'm trying to do here, I want to make it so that when the correct option is picked, it increases the value of the variable Bjorn pleased from 0 to 1
+// This in turn should then increase the BjornPleased, which will make it register in the function below.
+// Then finally when the selectOption function is (councilPleased >= 2) it will achieve the GameComplete
+
+function councilPleased() {
+    if (BjornPleased = 1) {
+        return 1;
+    }
+    else if (AstridPleased = 1) {
+        return 1;
+    }
+};
+
 // starts the game if the textNodeID is equal to or less than 0, and ends the game once the three states are met
 
 function selectOption(option) {
     const nextTextNodeId = option.nextText;
     if (nextTextNodeId <= 0) {
         return startGame();
-    } else if (state == {BjornPleased: true, AstridPleased: true}) {
+    } else if (councilPleased >= 2 ) {
         return GameComplete();
     }
     // takes the state we have currently, adds everything from option.setState, and overrides everything in the state
     // ie if BjornPleased is true in state, but false in option.setState, it will set it to false in our state
     // This will return a brand new object and set it to our current state
-    state = Object.assign(state + option.setState);
+    //             state = Object.assign(state, option.setState);
     showTextNode(nextTextNodeId);
 }
 
@@ -117,7 +136,7 @@ const textNodes = [
         dialogue: "Great. Happy to have you",
         
         options: [
-            {   text: "Yes",    setState: { AstridPleased: true },   nextText: 1,   },
+            {   text: "Yes",    BjornPleased: 1,   nextText: 1,   },
         ]
     },
     {
@@ -126,13 +145,13 @@ const textNodes = [
         dialogue: "Great. Happy to have you",
         
         options: [
-            {   text: "Yes",    setState: { BjornPleased: true },   nextText: 1,    },
+            {   text: "Yes",    AstridPleased: 1,   nextText: 1,    },
         ]
     },
     {
         id: 300,
         text: "Game Complete",
-        dialogue: "Yay you did it!",
+        dialogue: "Well done!!!!",
         options: [
             {   text: "Yes",    nextText: 1,    },
         ]
